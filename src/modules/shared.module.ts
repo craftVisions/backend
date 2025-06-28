6d317cb147d6cb093d65bf83ff0a4069193a235f
Sunny-64 <sunny6464n@gmail.com>
2025-06-29 00:24:22 +0530

    feat: modules setup

--- src/modules/shared.module.ts
+++ src/modules/shared.module.ts
@@ -3,10 +3,12 @@ import { JwtService } from "@nestjs/jwt";
 import { PassportModule } from "@nestjs/passport";
 import { DatabaseModule } from "src/db/database.module";
 import { Mailer } from "src/lib/mailer/mailer.service";
+import { QuestionModule } from './question/question.module';
+import { SubmissionModule } from './submission/submission.module';
 
 @Global()
 @Module({
-    imports: [DatabaseModule, PassportModule],
+    imports: [DatabaseModule, PassportModule, QuestionModule, SubmissionModule],
     providers: [JwtService, Mailer],
     exports: [DatabaseModule, PassportModule, Mailer, JwtService],
 })
